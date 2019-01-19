cf8903c077425a1ce26df98b5167fc61f48fb4fc
Kate Green <4kategreen@gmail.com>
2019-01-19 12:58:22 -0500

    something casey did

--- main.go
+++ main.go
@@ -1,44 +1,111 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
 	"os/signal"
 	"reflect"
 	"sync"
+	"syscall"
 	"time"
 )
 
+type Event struct {
+	Message string
+	Producer string
+	Timestamp time.Time
+}
+
+var eventChannel chan Event
+
+func init() {
+	eventChannel = make(chan Event, 100)
+}
+
+func event(message string, actor string) {
+	e := Event { Message: message, Producer: actor, Timestamp: time.Now() }
+	select {
+	case eventChannel <- e:
+		break
+	default:
+		fmt.Fprintf(os.Stderr, "Could not publish event; %v\n", e)
+	}
+}
+
 type Result struct {
+	success bool
+	stdout string
+	stderr string
+	message string
+	commandId string
 }
 
 type DispatchedCommand struct {
-	cmd string
+	Cmd []string
+}
+
+func (dc *DispatchedCommand) Id() string {
+	return buildId(dc.Cmd)
 }
 
 type RequestedCommand struct {
-	cmd string
+	Cmd []string
 }
 
 func (rc *RequestedCommand) dispatch() *DispatchedCommand {
 	return &DispatchedCommand {
-		cmd: rc.cmd,
+		Cmd: rc.Cmd,
 	}
 }
 
+func buildId(components []string) string {
+	id := ""
+	for _, c := range components {
+		id = id + c
+	}
+	return id
+}
+
+func (rc *RequestedCommand) Id() string {
+	return buildId(rc.Cmd)
+}
+
 type Dispatcher struct {
-	in <-chan *RequestedCommand
-	feedback <-chan *Result
-	out chan<- *DispatchedCommand
+	In <-chan *RequestedCommand
+	Feedback <-chan *Result
+	Out chan<- *DispatchedCommand
+	Budget map[string]uint
+
 	stop chan struct{}
+}
+
+func (d *Dispatcher) buildLedger() map[string] uint {
+	ledger := make(map[string] uint)
+	for k, _ := range d.Budget {
+		ledger[k] = 0
+	}
+	return ledger
 }
 
 func (d *Dispatcher) Start() {
 	d.stop = make(chan struct{}, 1)
+	ledger := d.buildLedger()
+
 	loop: for {
 		select {
-		case requested := <-d.in:
-			d.out <- requested.dispatch()
+		case result := <-d.Feedback:
+			ledger[result.commandId] = ledger[result.commandId] - 1
+			continue loop
+		case requested := <-d.In:
+			id := requested.Id()
+			if d.Budget[id] > ledger[id] {
+				ledger[id] = ledger[id] + 1
+				d.Out <- requested.dispatch()
+			} else {
+				event(fmt.Sprintf("Budget exceeded. Dropping %q", id), "Dispatcher")
+			}
 		case <- d.stop:
 			break loop
 		}
@@ -51,8 +118,9 @@ func (d *Dispatcher) Stop() {
 
 type WorkerPool struct {
 	DesiredCount int
-	in <-chan *DispatchedCommand
-	out chan<- *Result
+	In <-chan *DispatchedCommand
+	Out chan<- *Result
+
 	stop chan struct{}
 }
 
@@ -65,8 +133,34 @@ func (wp *WorkerPool) Start() {
 		go func() {
 			loop: for {
 				select {
-				case command := <- wp.in:
-					fmt.Printf("Working on %q\n", command.cmd)
+				case command := <- wp.In:
+					id := command.Id()
+					name := command.Cmd[0]
+					args := []string{}
+					if len(command.Cmd) > 1 {
+						args = command.Cmd[1:]
+					}
+
+					subProcess := exec.Command(name, args...)
+					subProcess.SysProcAttr = &syscall.SysProcAttr { Setpgid: true }
+					var stdout, stderr bytes.Buffer
+					subProcess.Stdout = &stdout
+					subProcess.Stderr = &stderr
+
+					var message string
+					subProcess.Start()
+					if err := subProcess.Wait(); err != nil {
+						if exitErr, ok := err.(*exec.ExitError); ok {
+							if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
+								message = fmt.Sprintf("Subprocess exited with nonzero status (%d)\n", status.ExitStatus())
+							}
+						} else {
+							message = fmt.Sprintf("Error invoking %q: %s\v\n", name, err)
+						}
+						wp.Out <- &Result{success: false, stdout: stdout.String(), stderr: stderr.String(), message: message, commandId: id}
+						continue loop
+					}
+					wp.Out <- &Result{success: true, stdout: stdout.String(), stderr: stderr.String(), commandId: id}
 				case <- wp.stop:
 					break loop
 				}
@@ -82,9 +176,10 @@ func (wp *WorkerPool) Stop() {
 }
 
 type ClockBasedTrigger struct {
+	Cmd RequestedCommand
+	Out chan *RequestedCommand
+
 	ticker time.Ticker
-	cmd RequestedCommand
-	out chan *RequestedCommand
 	stop chan struct{}
 }
 
@@ -94,14 +189,14 @@ func (cbt *ClockBasedTrigger) Start() {
 		select {
 		case <-cbt.stop:
 			cbt.ticker.Stop()
-			close(cbt.out)
+			close(cbt.Out)
 			break loop
 		case <-cbt.ticker.C:
 			select {
-			case cbt.out <- &cbt.cmd:
-				fmt.Printf("Queued %q\n", cbt.cmd)
+			case cbt.Out <- &cbt.Cmd:
+				event(fmt.Sprintf("Queued %q", cbt.Cmd), fmt.Sprintf("Clock Based Trigger (%s)", cbt.Cmd.Id()))
 			default:
-				fmt.Printf("Dropped %q since it is already scheduled. Consider decreasing the frequency of this task.\n", cbt.cmd)
+				event(fmt.Sprintf("Dropped %q since it is already scheduled. Consider decreasing the frequency of this task.", cbt.Cmd), fmt.Sprintf("Clock Based Trigger (%s)", cbt.Cmd.Id()))
 			}
 		}
 	}
@@ -112,26 +207,29 @@ func (cbt *ClockBasedTrigger) Stop() {
 }
 
 type AgendaEntry struct {
-	period time.Duration
-	command RequestedCommand
+	Period time.Duration
+	Command RequestedCommand
+	ConcurrencyBudget uint
 }
 
 type Agenda struct {
-	entries []AgendaEntry
+	Entries []AgendaEntry
+	Out chan<- *RequestedCommand
+
 	stop chan struct{}
 }
 
-func (a *Agenda) Start(output chan<- *RequestedCommand) {
+func (a *Agenda) Start() {
 	a.stop = make(chan struct{}, 1)
 	var wg sync.WaitGroup
 	var cbts []*ClockBasedTrigger
-	for _, entry := range a.entries {
+	for _, entry := range a.Entries {
 		wg.Add(1)
 
 		var cbt = &ClockBasedTrigger {
-			ticker: *time.NewTicker(entry.period),
-			cmd: entry.command,
-			out: make(chan *RequestedCommand, 1),
+			ticker: *time.NewTicker(entry.Period),
+			Cmd: entry.Command,
+			Out: make(chan *RequestedCommand, 1),
 		}
 
 		cbts = append(cbts, cbt)
@@ -147,7 +245,7 @@ func (a *Agenda) Start(output chan<- *RequestedCommand) {
 	go func() {
 		cases := make([]reflect.SelectCase, len(cbts))
 		for i, cbt := range cbts {
-			cases[i] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(cbt.out)}
+			cases[i] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(cbt.Out)}
 		}
 
 		remaining := len(cases)
@@ -167,10 +265,10 @@ func (a *Agenda) Start(output chan<- *RequestedCommand) {
 
 				continue
 			}
-			output <- value.Interface().(*RequestedCommand)
+			a.Out <- value.Interface().(*RequestedCommand)
 		}
 
-		close(output)
+		close(a.Out)
 		wg.Done()
 	}()
 
@@ -181,43 +279,129 @@ func (a *Agenda) Stop() {
 	close(a.stop)
 }
 
+func (a * Agenda) ConcurrencyBudget() map[string]uint {
+	budget := make(map[string]uint)
+	for _, e := range a.Entries {
+		budget[e.Command.Id()] = e.ConcurrencyBudget
+	}
+	return budget
+}
+
+type ResultsAggregator struct {
+	In <-chan *Result
+	Out chan<- *Result
+
+	stop chan struct{}
+}
+
+func (ra *ResultsAggregator) Start() {
+	ra.stop = make(chan struct{}, 1)
+	loop: for {
+		select {
+		case result := <-ra.In:
+			event(fmt.Sprintf("Got result: %q", result), "Results Aggregator")
+			ra.Out <- result
+		case <- ra.stop:
+			break loop
+		}
+	}
+}
+
+func (ra *ResultsAggregator) Stop() {
+	close(ra.stop)
+}
+
+type EventPublisher struct {
+	In <-chan Event
+
+	stop chan struct {}
+}
+
+func (ep *EventPublisher) Start() {
+	ep.stop = make(chan struct{}, 1)
+	loop: for {
+		select {
+		case e := <-ep.In:
+			fmt.Printf("%v\n", e)
+		case <- ep.stop:
+			break loop
+		}
+	}
+
+	for e := range ep.In {
+		fmt.Printf("%v\n", e)
+	}
+}
+
+func (ep *EventPublisher) Stop() {
+	close(ep.stop)
+}
+
 var exampleAgenda = Agenda {
-	entries: []AgendaEntry{
-		AgendaEntry { period: 2 * time.Second, command: RequestedCommand { cmd: "ls" } },
-		AgendaEntry { period: 3 * time.Second, command: RequestedCommand { cmd: "echo 'hello'" } },
+	Entries: []AgendaEntry{
+		AgendaEntry { Period:  2 * time.Second, Command: RequestedCommand { Cmd: []string{"ls"} }, ConcurrencyBudget: 1 },
+		AgendaEntry { Period:  3 * time.Second, Command: RequestedCommand { Cmd: []string{"echo", "hello"} }, ConcurrencyBudget: 5 },
+		AgendaEntry { Period:  5 * time.Second, Command: RequestedCommand { Cmd: []string{"sleep", "9"} }, ConcurrencyBudget: 1 },
+  	AgendaEntry { Period: 10 * time.Second, Command: RequestedCommand { Cmd: []string{"non-existent command"} }, ConcurrencyBudget: 1 },
 	},
 }
 
 func main() {
 	var wg sync.WaitGroup
+	var eventWg sync.WaitGroup
 
 	requestedCommands := make(chan *RequestedCommand)
 	dispatchedCommands := make(chan *DispatchedCommand)
+	rawResults := make(chan *Result)
+	feedback := make(chan *Result, 2 * len(exampleAgenda.Entries))
 
-	var dispatcher = Dispatcher {in: requestedCommands, out: dispatchedCommands}
-	var workerPool = WorkerPool {DesiredCount: 5, in: dispatchedCommands}
+	exampleAgenda.Out = requestedCommands
+	var dispatcher = Dispatcher {In: requestedCommands, Out: dispatchedCommands, Feedback: feedback, Budget: exampleAgenda.ConcurrencyBudget()}
+	var workerPool = WorkerPool {DesiredCount: 5, In: dispatchedCommands, Out: rawResults}
+	var resultsAggregator = ResultsAggregator { In: rawResults, Out: feedback }
+	var eventPublisher = EventPublisher {In: eventChannel}
 
 	wg.Add(1)
 	go func() {
 		workerPool.Start()
+		resultsAggregator.Stop()
+		event("Worker Pool stopped", "main")
 		wg.Done()
 	}()
 
 	wg.Add(1)
 	go func() {
 		dispatcher.Start()
+		event("Dispatcher stopped", "main")
 		wg.Done()
 	}()
 
 	wg.Add(1)
 	go func() {
-		exampleAgenda.Start(requestedCommands)
+		exampleAgenda.Start()
+		event("Agenda stopped", "main")
 		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		resultsAggregator.Start()
+		event("Results Aggregator stopped", "main")
+		wg.Done()
+	}()
+
+	eventWg.Add(1)
+	go func() {
+		eventPublisher.Start()
+		eventWg.Done()
 	}()
 
 	done := make(chan struct{})
 	go func() {
 		wg.Wait()
+		close(eventChannel)
+		eventPublisher.Stop()
+		eventWg.Wait()
 		close(done)
 	}()
 
@@ -226,12 +410,28 @@ func main() {
 
 	wait: for {
 		select {
-		case <-done:
+ 		case <-done:
+			fmt.Printf("Graceful shutdown complete\n")
 			break wait
 		case <-interrupt:
+			event("Starting graceful shutdown", "main")
 			exampleAgenda.Stop()
 			workerPool.Stop()
 			dispatcher.Stop()
 		}
 	}
 }
+
+// TASKS
+//   x Determine success/failure via exit code
+//   x Build results aggregator
+//   x Define concurrency limit in Agenda Entries
+//   x Handle feedback and budgets in dispatcher
+//   x Define "event" (or "state transition") listener. (All of these events will be forwarded to datadog)
+//   - Move each actor to a separate file
+//   - Move all message datatype declarations to a separate file
+
+// Post Prototype Tasks
+//   - Define an actual datatype for "ids" and ensure that they are unique
+//   - Allow external configuration of agenda
+//   - Sigusr to increase/decrease verbosity of events to datadog
